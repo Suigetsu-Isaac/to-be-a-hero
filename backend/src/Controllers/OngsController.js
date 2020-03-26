5aad1277af8df2a8eddf4a8e50455ec2e8da0189
Suigetsu-Isaac <isaacantonioalves@gmail.com>
2020-03-26 15:49:29 -0300

    Front-end do App

--- backend/src/Controllers/OngsController.js
+++ backend/src/Controllers/OngsController.js
@@ -4,13 +4,13 @@ const connection = require('../database/connection');
 module.exports = {
     async index(request, response) {
         const ongs = await connection('ongs').select('*')
-
+        console.log('entrei');
         return response.json(ongs);
     },
 
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
-
+        console.log('entrei');
         const id = crypto.randomBytes(4).toString('HEX');
 
         await connection('ongs').insert({
