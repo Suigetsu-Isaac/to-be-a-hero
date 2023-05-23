d7ac5434e8042176ae8076bb4f8e9d798fc2490f
IsaacSouzaTSI <59900989+Suigetsu-Isaac@users.noreply.github.com>
2023-05-23 12:37:19 -0300

    Update api.js
    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-require('dotenv/config');
-
 const url = "https://to-be-a-hero.onrender.com"
-const envUrl = process.env.REACT_APP_API_URL
+
 const api = axios.create({
     baseURL : url,
 })
